8ef197fdebcc2e7d6eec7afe9405585c1ffcc13f
aleolivera <62443592+aleolivera@users.noreply.github.com>
2022-04-02 19:47:35 -0300

    limpieza de codigo en main otra vez

--- src/view/Main.java
+++ src/view/Main.java
@@ -33,11 +33,11 @@ public class Main {
 		entradas.add(entradaInfantil2);
 		
 		              //Evento Deportivo
-		EventoDeportivo eventoDeportivo = new EventoDeportivo("Final Copa América", Duration.ofMinutes(90), LocalDate.of(2022, 8, 25), new Deporte("futbol"), true);	
+		EventoDeportivo eventoDeportivo = new EventoDeportivo("Final Copa América", Duration.ofMinutes(90), LocalDate.of(2022, 8, 25),new Deporte("futbol"), true);	
 		Entrada entradaDeportiva = eventoDeportivo.crearEntrada(new Tipo(1,"futbol"));	
 		entradas.add(entradaDeportiva);
 		
-		EventoDeportivo eventoDeportivo2 = new EventoDeportivo("Campeonato argentino", Duration.ofMinutes(80), LocalDate.of(2022, 12, 15), new Deporte("Rugby"), true);	
+		EventoDeportivo eventoDeportivo2 = new EventoDeportivo("Campeonato argentino", Duration.ofMinutes(80), LocalDate.of(2022, 12, 15),new Deporte("Rugby"), true);	
 		Entrada entradaDeportiva2 = eventoDeportivo2.crearEntrada(new Tipo(2,"Rugby"));	
 		eventoDeportivo2.setInternacional(false);  
 		entradas.add(entradaDeportiva2);
@@ -56,7 +56,10 @@ public class Main {
 		}
 		else
 		{
-			EventoTeatro eventoTeatro = new EventoTeatro(actores, new Genero ("Dramaturgo"),"Fausto", Duration.ofMinutes(50), LocalDate.of(2022, 4, 6));
+			EventoTeatro eventoTeatro = new EventoTeatro(
+						actores, new Genero ("Dramaturgo"),"Fausto", 
+						Duration.ofMinutes(50), LocalDate.of(2022, 4, 6)
+					);
 			Entrada entradaTeatro = eventoTeatro.crearEntrada(new Tipo(1, "Teatro" ));
 			entradas.add(entradaTeatro);
 		}
@@ -74,7 +77,10 @@ public class Main {
 		}
 		else
 		{
-			EventoTeatro eventoTeatro2 = new EventoTeatro(actores2, new Genero ("Comedia"),"mucho ruido pocas nueces", Duration.ofMinutes(50), LocalDate.of(2022, 10, 6));
+			EventoTeatro eventoTeatro2 = new EventoTeatro(
+						actores2, new Genero ("Comedia"),"mucho ruido pocas nueces", 
+						Duration.ofMinutes(50), LocalDate.of(2022, 10, 6)
+					);
 			Entrada entradaTeatro2 = eventoTeatro2.crearEntrada(new Tipo(1, "Teatro" ));
 			entradas.add(entradaTeatro2);
 		}
@@ -88,7 +94,10 @@ public class Main {
 		bandaSopote.add(banda);
 		bandaSopote.add(banda2);
 		bandaSopote.add(banda3);
-		EventoRecital eventoRecital = new EventoRecital("Cosquín Rock",Duration.ofMinutes(120),LocalDate.of(2022, 10, 10), new Banda("los Beatles",new Genero("rock")),bandaSopote);
+		EventoRecital eventoRecital = new EventoRecital(
+					"Cosquín Rock",Duration.ofMinutes(120),LocalDate.of(2022, 10, 10), 
+					new Banda("los Beatles",new Genero("rock")),bandaSopote
+				);
 		Entrada entradaRecital = eventoRecital.crearEntrada(new Tipo(1,"generales"));
 		entradas.add(entradaRecital);
 
@@ -101,7 +110,10 @@ public class Main {
 		bandaSopote2.add(banda4);
 		bandaSopote2.add(banda5);
 		bandaSopote2.add(banda6);
-		EventoRecital eventoRecital2 = new EventoRecital("pop",Duration.ofMinutes(120),LocalDate.of(2022, 7, 7), new Banda("coldplay",new Genero("pop")),bandaSopote2);
+		EventoRecital eventoRecital2 = new EventoRecital(
+					"pop",Duration.ofMinutes(120), LocalDate.of(2022, 7, 7),
+					new Banda("coldplay",new Genero("pop")),bandaSopote2
+					);
 		Entrada entradaRecital2 = eventoRecital2.crearEntrada(new Tipo(2,"VIP"));
 		entradas.add(entradaRecital2);
 		
